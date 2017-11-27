bd7855708665f88c887fbc07deaa2ceb5dd0acd1
disha1010 <diana.saginbaeva@gmail.com>
2017-11-27 18:53:38 +0300

    fix bugs in main page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-$( document ).ready(function() {
-  $(".search-form__toggle").on("click", function() {
+$(document).ready(function () {
+  $(".search-form__toggle").on("click", function () {
     $(".header-wrapper").addClass("search-active");
   });
-  $(".search-form__close").on("click", function() {
+  $(".search-form__close").on("click", function () {
     $(".header-wrapper").removeClass("search-active");
   })
+  // $(".sub-menu-toggle").on("click", function () {
+  //   setTimeout(function () {
+  //     $(".menu-item.dropdown").addClass("open");
+  //     $(".navbar-link-item").attr("aria-expanded", "true");
+  //   })
+  // })
 });
